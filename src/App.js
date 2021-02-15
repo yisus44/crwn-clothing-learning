1e0f3063d2835f00de84800952abd0aa0d03c285
Jesus <jesusadrian1953.1@gmail.com>
2021-02-15 15:09:28 -0600

    Broken our homepage into smaller component. finished styling menu-item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+import Directory from "../../components/directory/directory.compontent";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory></Directory>
+    </div>
+  );
+};
+
+export default HomePage;
